463e6a529cab5c1cc437a6d7819f6dcd934816fd
shreyans-gits <shreyans.sahu07@gmail.com>
2026-04-09 10:27:52 +0530

    Phase 1 & 2

---
+++ README.md
@@ -0,0 +1,5 @@
+## Dataset
+Download the IMDB Dataset from Kaggle:
+https://www.kaggle.com/datasets/lakshmi25npathi/imdb-dataset-of-50k-movie-reviews
+
+Place it in the data/ folder as "IMDB Dataset.csv"
---
+++ data/preprocess.py
@@ -0,0 +1,81 @@
+import pandas as pd
+import numpy as np
+import re
+import nltk
+from nltk.corpus import stopwords
+from sklearn.datasets import fetch_20newsgroups
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+import joblib
+import os
+import re
+from nltk.corpus import stopwords
+from sklearn.model_selection import train_test_split
+
+stop_words = set(stopwords.words('english'))
+
+def load_data():
+    df = pd.read_csv("data/IMDB Dataset.csv")
+    X = df['review']
+    y = df['sentiment'].map({'positive': 1, 'negative': 0})
+    return X, y
+
+def clean_text(text):
+    text = text.lower()
+    text = re.sub(r'<.*?>', '', text)
+    text = re.sub(r'[^a-z\s]', '', text)
+    words = text.split()
+    words = [w for w in words if w not in stop_words]
+    text = ' '.join(words)
+    return text
+
+def clean_dataset(texts):
+    cleaned = []
+    for text in texts:
+        cleaned.append(clean_text(text))
+    return cleaned
+
+def split_data(texts, labels, test_size=0.2, random_state=42):
+    X_train, X_test, y_train, y_test = train_test_split(
+        texts,
+        labels,
+        test_size=test_size,
+        random_state=random_state,
+        stratify=labels
+    )
+
+    return X_train, X_test, y_train, y_test
+
+def vectorize(X_train, X_test):
+    vectorizer = TfidfVectorizer(max_features=5000)
+    X_train_tfidf = vectorizer.fit_transform(X_train)
+    X_test_tfidf = vectorizer.transform(X_test)
+    return X_train_tfidf, X_test_tfidf, vectorizer
+
+def save_artifacts(vectorizer,X_train,X_test,y_train,y_test):
+    joblib.dump(vectorizer,"data/vectorizer.pkl")
+    joblib.dump(X_train,"data/X_train.pkl")
+    joblib.dump(X_test,"data/X_test.pkl")
+    joblib.dump(y_train,"data/y_train.pkl")
+    joblib.dump(y_test,"data/y_test.pkl")
+
+def main():
+    print("Loading data...")
+    X, y = load_data()
+
+    print("Cleaning data...")
+    X_cleaned = clean_dataset(X)
+
+    print("Splitting data...")
+    X_train, X_test, y_train, y_test = split_data(X_cleaned, y)
+
+    print("Vectorizing...")
+    X_train_tfidf, X_test_tfidf, vectorizer = vectorize(X_train, X_test)
+
+    print("Saving artifacts...")
+    save_artifacts(vectorizer, X_train_tfidf, X_test_tfidf, y_train, y_test)
+
+    print("Done! All artifacts saved to data/")
+
+if __name__ == "__main__":
+    main()
