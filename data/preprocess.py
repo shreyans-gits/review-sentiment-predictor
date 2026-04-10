850996ed82c6fdb1a34dd836b4a81883a2c01113
tanisha2607 <tanishag.2607@gmail.com>
2026-04-10 21:47:14 +0530

    updated formatting

--- data/preprocess.py
+++ data/preprocess.py
@@ -7,7 +7,6 @@ from sklearn.datasets import fetch_20newsgroups
 from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 import joblib
-import os
 import re
 from nltk.corpus import stopwords
 from sklearn.model_selection import train_test_split
@@ -21,7 +20,7 @@ def load_data():
     y = df['sentiment'].map({'positive': 1, 'negative': 0})
     return X, y
 
-# covert text into lower case,remove space
+#covert text into lower case, remove space
 def clean_text(text):
     text = text.lower()
     text = re.sub(r'<.*?>', '', text)
@@ -38,7 +37,7 @@ def clean_dataset(texts):
         cleaned.append(clean_text(text))
     return cleaned
 
-# 80-20 mei divide krta hai
+#80% is for learning and 20% is for testing
 def split_data(texts, labels, test_size=0.2, random_state=42):
     X_train, X_test, y_train, y_test = train_test_split(
         texts,
@@ -50,14 +49,14 @@ def split_data(texts, labels, test_size=0.2, random_state=42):
 
     return X_train, X_test, y_train, y_test
 
-
+#creates a list of numbers that the model can understand
 def vectorize(X_train, X_test):
     vectorizer = TfidfVectorizer(max_features=5000)
     X_train_tfidf = vectorizer.fit_transform(X_train)
     X_test_tfidf = vectorizer.transform(X_test)
     return X_train_tfidf, X_test_tfidf, vectorizer
 
-
+#it stores what it learnt from the dataset in the memory
 def save_artifacts(vectorizer,X_train,X_test,y_train,y_test):
     joblib.dump(vectorizer,"data/vectorizer.pkl")
     joblib.dump(X_train,"data/X_train.pkl")
@@ -83,6 +82,5 @@ def main():
 
     print("Done! All artifacts saved to data/")
 
-#so that we can include above functions in any othe file
 if __name__ == "__main__":
     main()
